0187f9f26d6b9a501f4b9aff9b21589841aa6f17
Davit Melikadze <melikadze@live.com>
2024-09-02 22:12:06 +0200

    Update VueJS to 3.4.38

--- resources/js/Components/MarkdownEditor.vue
+++ resources/js/Components/MarkdownEditor.vue
@@ -84,19 +84,18 @@
 import StarterKit from '@tiptap/starter-kit';
 import { EditorContent, useEditor } from '@tiptap/vue-3';
 import { Markdown } from 'tiptap-markdown';
-import { watch } from 'vue';
+import { onMounted, watch } from 'vue';
 import 'remixicon/fonts/remixicon.css';
 import Link from '@tiptap/extension-link';
 import Placeholder from '@tiptap/extension-placeholder';
 
 
 const props = defineProps({
-    modelValue: '',
     editorClass: '',
     placeholder: null,
 });
 
-const emit = defineEmits(['update:modelValue']);
+const model = defineModel();
 
 const editor = useEditor({
     extensions: [
@@ -118,19 +117,22 @@ const editor = useEditor({
             class: `min-h-[512px] prose prose-sm max-w-none py-1.5 px-3 ${props.editorClass}`,
         },
     },
-    onUpdate: () => emit('update:modelValue', editor.value?.storage.markdown.getMarkdown())
+    onUpdate: () => model.value = editor.value?.storage.markdown.getMarkdown()
 });
 
 defineExpose({ focus: () => editor.value.commands.focus()});
 
-watch(() => props.modelValue, (value) => {
+onMounted( () => {
+    watch(model, (value) => {
 
     if (value === editor.value?.storage.markdown.getMarkdown()) {
         return;
     }
 
     editor.value?.commands.setContent(value);
-}, { immediate: true });
+    }, { immediate: true });
+});
+
 
 const promptUserForHref = () => {
 
